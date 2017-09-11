297adf5e4d55e35717d8f99522544ad3a170ffa6
InSunShine2015 <3284933048@qq.com>
2017-09-11 17:13:41 +0800

    增加commons字符串测试说明

--- src/main/java/com/sun/commons/StringUtil.java
+++ src/main/java/com/sun/commons/StringUtil.java
@@ -11,5 +11,21 @@ public class StringUtil {
 		StringUtils.defaultString(str, "$$");// null会替换为默认值，为防止字符串空指针
 		StringUtils.defaultString(str);// null-->""
 
+		// 分隔字符串为数组
+		// 即使分隔符中间无值也算作 对api的分隔有很好作用
+		String tosplit = "a|b|||";
+		String[] toArray = StringUtils.splitByWholeSeparatorPreserveAllTokens(tosplit, "|");// --a
+																							// b
+																							// 空串
+																							// 空串
+																							// 空串
+		for (String st : toArray) {
+			System.out.println("-->" + st + "<--");
+		}
+		String[] toArray1 = tosplit.split("\\|");// --a b
+		for (String st : toArray1) {
+			System.out.println("==>" + st + "<==");
+		}
+
 	}
 }
