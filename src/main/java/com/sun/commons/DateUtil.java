c5fe6d2ac301947d1a9396827189c83c77271d3e
sunxiaoguang <sunxiaoguang@chinabr.net>
2020-07-10 15:20:57 +0800

    增加java8时间和Date互转的工具类方法

--- src/main/java/com/sun/commons/DateUtil.java
+++ src/main/java/com/sun/commons/DateUtil.java
@@ -1,5 +1,9 @@
 package com.sun.commons;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.apache.commons.lang3.time.DateUtils;
@@ -21,4 +25,36 @@ public class DateUtil {
 		}
 		return date;
 	}
+	/**
+	 * LocalDate转为Date
+	 * @param localDate
+	 * @return
+	 */
+	public static Date asDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+	/**
+	 * localDateTime 转为Date
+	 * @param localDateTime
+	 * @return
+	 */
+    public static Date asDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+    /**
+     * Date转为LocalDate
+     * @param date
+     * @return
+     */
+    public static LocalDate asLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+    /**
+     * Date转为LocalDateTime
+     * @param date
+     * @return
+     */
+    public static LocalDateTime asLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
 }
